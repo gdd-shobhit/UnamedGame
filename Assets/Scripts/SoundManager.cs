286b336d9e9c2127382d3096a7aea068b6c80447
nineteenfrog <56976592+nineteenfrog@users.noreply.github.com>
2022-09-26 23:34:30 -0400

    Added Sound manager
    Created sound manager script and implemented singleton data structure.

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource musicSource, effectsSource;
+
+    //Singleton sound manager. A single sound manager will be created in the game
+    //and will manage both music and effects for the game.
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        effectsSource.PlayOneShot(clip);
+    }
+}
